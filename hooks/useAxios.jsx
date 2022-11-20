4e087f8a9cdb5f17c53cf103897a427f0e909f9d
congtrung6391 <congtrung6391@gmail.com>
2022-11-20 19:28:10 +0700

    replace env

--- hooks/useAxios.jsx
+++ hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = process.env.HOST;
 
 const useAxios = () => {
   const [response, setResponse] = useState(null);
